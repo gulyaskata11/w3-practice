a8e155a05a11b94ffdfb7b23718a13930b2b13c0
vombat11 <gulyas.kata11@gmail.com>
2022-09-14 11:42:52 +0200

    variables

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+let myVariable = "value";
+
+console.log(myVariable);
+
+myVariable = "new value";
+
+console.log(myVariable);
+
+let myVariable2 = "szöveg";
+
+console.log(myVariable2);
+
+myVariable = myVariable2;
+
+console.log(myVariable);
+
+let myVariable3 = myVariable + myVariable2;
+
+console.log(myVariable3)
