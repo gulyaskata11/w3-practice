2bb33419777b6c8b1f0b469a681e0f74a415497c
vombat11 <gulyas.kata11@gmail.com>
2022-09-14 12:13:32 +0200

    function, load event

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,35 @@
 let myVariable = "value";
 
-console.log(myVariable);
+//console.log(myVariable);
 
 myVariable = "new value";
 
-console.log(myVariable);
+//console.log(myVariable);
 
 let myVariable2 = "szöveg";
 
-console.log(myVariable2);
+// console.log(myVariable2);
 
 myVariable = myVariable2;
 
-console.log(myVariable);
+//console.log(myVariable);
 
 let myVariable3 = myVariable + myVariable2;
 
-console.log(myVariable3)
+//console.log(myVariable3)
+
+function sumVariables(variable1, variable2) {
+    console.log(variable1 + variable2)
+};
+
+sumVariables(8, 6);
+sumVariables("word1", "word2");
+sumVariables(myVariable, myVariable2)
+
+function loadEvent() {
+    console.log("loaded")
+    let rootElement = document.getElementById("root")
+    rootElement.insertAdjacentHTML("beforeend", "Hello word!")
+};
+
+window.addEventListener("click", loadEvent);
