ace390d0136df43c78f33be7a0bd9cbf9388859a
vombat11 <gulyas.kata11@gmail.com>
2022-09-15 11:46:48 +0200

    new function

--- frontend/script.js
+++ frontend/script.js
@@ -27,9 +27,14 @@ sumVariables("word1", "word2");
 sumVariables(myVariable, myVariable2)
 
 function loadEvent() {
-    console.log("loaded")
-    let rootElement = document.getElementById("root")
-    rootElement.insertAdjacentHTML("beforeend", "Hello word!")
+   const newCatButton = document.querySelector(".add-cat")
+   newCatButton.addEventListener("click", function () {
+    const catContainer = document.querySelector(".cats")
+    const now = new Date().getTime()
+    const newCat = `<img src="https://cataas.com/cat?random=${now}" alt="" />`
+    catContainer.insertAdjacentHTML("beforeend", newCat)
+   })
 };
 
-window.addEventListener("click", loadEvent);
+window.addEventListener("load", loadEvent);
+
